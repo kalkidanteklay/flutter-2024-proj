bf7a7736d641162e89567b3e2ec62c60e7cf7c6e
kalkidanteklay <kalkidanteklehaimanot@gmail.com>
2024-04-20 17:05:33 +0300

    edited route

--- flutter_pet_pal/lib/main.dart
+++ flutter_pet_pal/lib/main.dart
@@ -16,11 +16,7 @@ class MyApp extends StatelessWidget {
         home: SignupPage(),
         // theme: ThemeData(primarySwatch: Colors.orange),
         routes: {
-          '/intro_page': (context) => IntroPage(),
-          '/signup_page': (context) => SignupPage(),
-          '/user_page': (context) => UserPage(),
-          '/login_page': (context) => LoginPage(),
-          '/fav_page': (context) => FavoritesPage(),
+          '/admin': (context) => AdminPage(),
         });
   }
 }
