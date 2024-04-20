fbd8cd9326626f89e8a54a5d3c164de4e9c26238
bethel5 <betheldereje5@gmail.com>
2024-04-20 12:59:25 +0300

    signup and userpage

---
+++ flutter_pet_pal/lib/presentation/screens/signup_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pet_pal/presentation/widgets/button.dart';
+import 'package:flutter_pet_pal/presentation/widgets/my_textfield.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  // text editing controllers
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  // String userType;
+
+  // void checkUserType(String email) {
+  //   if (email.endsWith('@example.com')) {
+  //     setState(() {
+  //       userType = 'admin';
+  //     });
+  //   } else {
+  //     setState(() {
+  //       userType = 'user';
+  //     });
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const SizedBox(height: 10),
+                //image
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 100, left: 100, bottom: 5),
+                  child: Image.asset(
+                    'lib/images/signup.png',
+                    scale: 5,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+                //text
+                const Text(
+                  "P E T P A L    A D O P T I O N",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+
+                const SizedBox(height: 25),
+                //textfield full name
+                MyTextfield(
+                    controller: nameController,
+                    hintText: "Enter your full name",
+                    obsecureText: false),
+
+                const SizedBox(height: 25),
+                //textfield email
+                MyTextfield(
+                    controller: emailController,
+                    hintText: "Enter your email",
+                    obsecureText: false),
+
+                const SizedBox(height: 25),
+                //textfield password
+                MyTextfield(
+                    controller: passwordController,
+                    hintText: "Enter your password",
+                    obsecureText: true),
+
+                const SizedBox(height: 25),
+                //textfield to confirm password
+                MyTextfield(
+                    controller: passwordController,
+                    hintText: "Confirm Password",
+                    obsecureText: true),
+
+                const SizedBox(height: 30),
+                //signup button
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: MyButton(
+                    text: "Sign Up", // ontap:
+                    onTap: () {
+                      Navigator.pushNamed(context, '/user_page');
+                    },
+                  ),
+                ),
+
+                const SizedBox(height: 7),
+                //final message
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    "Congratulations on taking the first step towards finding your new furry friend!",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_pet_pal/lib/presentation/widgets/tab/dog_tab.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pet_pal/presentation/widgets/all_tiles.dart';
+
+class DogsTab extends StatelessWidget {
+  List dogsAvailable = [
+    //[image, name, age, gender, species]
+    [
+      'lib/images/dog1.jpeg',
+      "Name: Max",
+      'Age: 3',
+      'Gender: Male',
+      'Species: Labrador Retriever',
+    ],
+    [
+      'lib/images/dog2.jpeg',
+      "Name :Luna",
+      'Age: 2',
+      'Gender: Female',
+      'Species: German Shepherd',
+    ],
+    [
+      'lib/images/dog3.jpeg',
+      "Name: Bobby",
+      'Age: 5',
+      'Gender: Male',
+      'Species: Golden Retriever',
+    ],
+    [
+      'lib/images/dog4.jpeg',
+      "Name: Daisy",
+      'Age: 1',
+      'Gender: Female',
+      'Species: Bulldog',
+    ],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      
+      padding: EdgeInsets.all(12),
+      itemCount: dogsAvailable.length,
+
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, childAspectRatio: 0.5),
+      itemBuilder: (context, index) {
+        return AllTile(
+          imagePath: dogsAvailable[index][0],
+          name: dogsAvailable[index][1],
+          age: dogsAvailable[index][2],
+          gender: dogsAvailable[index][3],
+          species: dogsAvailable[index][4],
+        );
+      },
+    );
+  }
+}
