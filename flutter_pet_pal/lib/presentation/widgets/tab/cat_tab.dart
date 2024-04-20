45a6efc05fca9951853ff36ae5c1acaf493af7c9
Bethel5 <149252303+Bethel5@users.noreply.github.com>
2024-04-20 18:48:04 +0300

    Update cat_tab.dart

--- flutter_pet_pal/lib/presentation/widgets/tab/cat_tab.dart
+++ flutter_pet_pal/lib/presentation/widgets/tab/cat_tab.dart
@@ -54,12 +54,3 @@ class CatsTab extends StatelessWidget {
     );
   }
 }
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Center(child: Text('Cats Tab')),
-//     );
-//   }
-// }
